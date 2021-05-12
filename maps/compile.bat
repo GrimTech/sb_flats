62b8df93a999b5963b6ac872b7cb3820f5ab3573
Patrick Jr <patrickcaulton423@gmail.com>
2021-05-12 23:54:06 +0100

    Testing

---
+++ maps/compile.bat
@@ -0,0 +1,3 @@
+D:\Steam\steamapps\common\sbox\bin\win64\resourcecompiler.exe -fshallow -maxtextureres 4096 -dxlevel 110 -quiet -unbufferedio -noassert -i "d:/steam/steamapps/common/sbox/addons/sb_flats/maps/flats.vmap"  -world -bakelighting -lightmapMaxResolution 4096 -vrad3 -lightmapDoWeld -lightmapVRadQuality 2 -lightmapLocalCompile -lightmapCompressionDisabled 0 -phys -sareverb -sapaths -breakpad  -nompi  -nop4 -outroot "D:\Steam\steamapps\common\sbox\addons"
+
+pause
